f29c7493b756cd35482c0d88ae3ebd100befc0cb
Mubarak Mohammed <mshehujrn@gmail.com>
2026-05-12 14:42:02 +0100

    modified model.py

--- model/model.py
+++ model/model.py
@@ -4,19 +4,7 @@ import torch.nn as nn
 from torchvision import models
 
 def Gas_leak_model():
-    # These four lines must be indented by exactly 4 spaces (one Tab)
     model = models.resnet18(weights=None)
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 2)
-    return model
-
-# If you want to initialize it here for use within this file:
-# model = Gas_leak_model()
-from torchvision import models
-
-    
-    # You MUST redefine the final layer exactly as you did during training
-    num_ftrs = model.fc.in_features
-    model.fc = nn.Linear(num_ftrs, 2) # Assuming 2 classes: Leak/No Leak
-    
     return model
